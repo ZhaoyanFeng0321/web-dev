d7b7330ea4f643fd7d9221dc67fe397892433a91
Zhaoyan <irisfeng0321@outlook.com>
2022-04-05 23:35:41 -0400

    fix bugs

--- src/components/tuiter/HomeScreen/index.js
+++ src/components/tuiter/HomeScreen/index.js
@@ -5,18 +5,11 @@ import '../index.css';
 const HomeScreen = () => {
     return (
         <>
-                {/*<div*/}
-                {/*    id="tuiter-nav"*/}
-                {/*    className="col-2 col-sm-3 col-md-2 col-lg-2 col-xl-2 col-xxl-2"*/}
-                {/*>*/}
-                {/*    <NavigationSidebar active="Home"/>*/}
-                {/*</div>*/}
+
                 <div id="wd-home-body">
                     <PostList/>
                 </div>
-                {/*<div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 col-xxl-3">*/}
-                {/*    <PostSummaryList/>*/}
-                {/*</div>*/}
+
         </>
     );
 }
--- src/components/tuiter/NavigationSidebar/NavItem.js
+++ src/components/tuiter/NavigationSidebar/NavItem.js
@@ -6,8 +6,8 @@ const NavItem = ({
     return (
 
         <Link to={link.path}
-              className="list-group-item list-group-item-action wd-list-group-item-override"
-              {`${active === link.label ? 'active' : ''}`}>
+              className="list-group-item list-group-item-action wd-list-group-item-override">
+              {`${active === link.label ? 'active' : ''}`}
             <i className={link.icon}></i>
             <span className="d-none d-xl-inline-block">  {link.label}</span>
         </Link>
